8c3d6a8bc3f82afeb5498ef29546127ece5c5931
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-02-24 22:07:52 -0500

    Add string variables

---
+++ variables.js
@@ -0,0 +1,8 @@
+var email = 'lidiakarpinski@gmail.com';
+//variable email assigned to string 'lidiakarpinski@gmail.com'
+
+var greeting = 'hello!';
+//variable greeting assigned to string 'hello!'
+
+var username = 'lkarpins';
+//variable username assigned to string 'lkarpins'
