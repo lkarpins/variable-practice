17f80f23159fe0264901944090799fc553710c39
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-02-24 22:12:47 -0500

    Add number variables

--- variables.js
+++ variables.js
@@ -6,3 +6,12 @@ var greeting = 'hello!';
 
 var username = 'lkarpins';
 //variable username assigned to string 'lkarpins'
+
+var age = 32;
+//variable age assigned to number 32
+
+var gpa = 4.0
+//variable gpa assigned to number 4.0
+
+var birthday = 914
+//variable birthday assigned to number 914
